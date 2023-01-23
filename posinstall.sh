8a73313c129147b6d7795aa683feba92d59e2695
moisesreis <moises.andrade@brlink.com.br>
2023-01-23 09:43:54 -0300

    inclusão das linhas de instalação dos pacotes git, got e yay

--- posinstall.sh
+++ posinstall.sh
@@ -12,8 +12,6 @@ PACKAGES_INSTALL=(
   dmenu
   firefox-developer-edition
   fzf
-  git
-  go
   make
   neovim
   nodejs
@@ -31,14 +29,35 @@ PACKAGES_INSTALL=(
   zsh
 )
 
+## Remover travas do pacman
+sudo rm /var/lib/pacman/db.lck
 
-sudo pacman -S git go
+## Instalar o pacote git
+if ! pacman -Qet | grep -q git; then
+  sudo pacman -S git --noconfirm
+else
+  echo "Git - Instalado"
+fi
 
+## Instalar o pacote go
+if ! pacman -Qet | grep -q git; then
+  sudo pacman -S go --noconfirm
+else
+  echo "Go - Instalado"
+fi
 
-mkdir "$DOWNLOAD_DIRECTORY"
+mkdir "$DOWNLOAD_DIRECTORY" 2> /dev/null
+
+git clone $URL_YAY $DOWNLOAD_DIRECTORY 2> /dev/null
+
+cd $DOWNLOAD_DIRECTORY/
+makepkg -si --noconfirm
+
 
 
 ## bmenu
 ## docker
 ## yay
 ## kubernets
+#
+## go
