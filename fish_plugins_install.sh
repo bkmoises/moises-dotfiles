3c9b4d5ab3d265124ffe6111380c74c3e5f8a334
bkmoises <bk_moises@hotmail.com>
2023-01-24 08:18:54 -0300

    inclusão dos plugins faltantes do fish

--- fish_plugins_install.sh
+++ fish_plugins_install.sh
@@ -5,3 +5,16 @@ omf install mars
 fisher install jethrokuan/z
 fisher install PatrickF1/fzf.fish
 fisher install rstacruz/fish-asdf
+#!/bin/fish
+
+omf update
+omf install mars
+fisher install jethrokuan/z
+fisher install PatrickF1/fzf.fish
+fisher install rstacruz/fish-asdf
+fisher install gazorby/fish-abbreviation-tips
+fisher install jorgebucaran/autopair.fish
+fisher install nickeb96/puffer-fish
+fisher install patrickf1/colored_man_pages.fish
+fisher install ankitsumitg/docker-fish-completions
+
