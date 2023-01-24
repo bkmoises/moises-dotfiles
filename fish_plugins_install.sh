3b31d16976aea42bfadd34b8f222b0ec3d18e3f5
bkmoises <bk_moises@hotmail.com>
2023-01-24 08:26:34 -0300

    correção de código duplicado

--- fish_plugins_install.sh
+++ fish_plugins_install.sh
@@ -2,13 +2,7 @@
 
 omf update
 omf install mars
-fisher install jethrokuan/z
-fisher install PatrickF1/fzf.fish
-fisher install rstacruz/fish-asdf
-#!/bin/fish
 
-omf update
-omf install mars
 fisher install jethrokuan/z
 fisher install PatrickF1/fzf.fish
 fisher install rstacruz/fish-asdf
