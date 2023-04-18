26fd071d11b0325282075d0e4a8870e0a79dd452
moisesreis <moises.andrade@brlink.com.br>
2023-04-18 15:30:06 -0300

    inclusão to instalador do fisher e alteração do shell principal

--- wsl-poisntall.sh
+++ wsl-poisntall.sh
@@ -29,12 +29,17 @@ packages=(
 sudo pacman -Syu
 
 for package in ${packages[@]}; do
-  if ! sudo pacman -Qet | grep -q $package; then
+  if ! sudo pacman -Qet | grep -q "^$package\$"; then
     sudo pacman -S $package --noconfirm
   else
     echo "$package - Já instalado"
   fi
 done
 
+# Instalar Fisherman
+curl -Lo ~/.config/fish/functions/fisher.fish --create-dirs https://git.io/fisher
+
+# Definir o Fish como shell padrão
+chsh -s /usr/bin/fish
 
 echo "Instalação completa!"
