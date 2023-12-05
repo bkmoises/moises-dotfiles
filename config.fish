2839aaf096153ec7e012fc2fc4433175178d5b25
Moisés Andrade <bk_moises@hotmail.com>
2023-12-05 09:23:21 -0300

    Add files via upload

---
+++ config.fish
@@ -0,0 +1,12 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+
+# ALIAS
+alias vi nvim
+alias ai aichat
+alias start 'cmd.exe /c start'
+
+# PATH
+set -x PATH $PATH /home/moisesreis/.local/bin /home/moisesreis/.cargo/bin
