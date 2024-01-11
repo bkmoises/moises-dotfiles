21b31364812f493c512abffa9fa3d2620348c54b
bkmoises <bk_moises@hotmail.com>
2024-01-11 11:08:42 -0300

    incusão de novos pacots ubuntu

--- Fish/config.fish
+++ Fish/config.fish
@@ -13,4 +13,3 @@ alias cls clear
 set -x JAVA_HOME /usr/lib/jvm/java-11-openjdk/
 set -x PATH $PTH /home/moisesreis/.local/bin /home/moisesreis/.cargo/bin $JAVA_HOME
 set -x SSH_AGENT_PID (ssh-agent -c)
-
