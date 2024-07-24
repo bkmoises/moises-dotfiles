c91ea4f17e3141d63dd879f9bcb5735de66f591c
bkmoises <bk_moises@hotmail.com>
2024-07-24 14:26:42 -0300

    feature: inclusão dos primeiros pacotes e alteração do shell

--- Ubuntu/teste.sh
+++ Ubuntu/teste.sh
@@ -12,4 +12,44 @@ function loading() {
 	done
 	echo ""
 }
-loading "Inicializando"
+
+loading "Atualizando sistema"
+sudo apt update -y
+sudo apt upgrade -y
+sudo apt dist-upgrade -y
+sudo apt autoremove -y
+
+packages=(
+  build-essential
+  cargo
+	nodejs
+	npm
+	python3
+  python-pip
+  curl
+  make
+  neovim
+	ssh
+	tig
+	tmux
+	tree
+	unrar
+	unzip
+	wget
+	yarn
+  bpytop
+  diodon
+	alacritty
+	discord
+	fish
+	fzf
+  flameshot
+	qbittorrent
+	vlc
+  bat
+  exa
+)
+
+sudo apt install -y "${packages[@]}"
+
+sudo chsh -s /usr/bin/fish
