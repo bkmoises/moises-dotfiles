41d841e3a7a4e1ad09d7e995dd40f78a9293e4da
bkmoises <bk_moises@hotmail.com>
2024-07-24 18:25:58 -0300

    feature: inclusão do instalador de plugins do fish

--- Ubuntu/install.sh
+++ Ubuntu/install.sh
@@ -2,3 +2,7 @@ source ../resources/functions.sh
 
 loading "Instalando pacotes"
 ./packages-install.sh
+loading "Aplicando configurações"
+./conf.sh
+loading "Instalando fish plugins"
+../resources/fish/
