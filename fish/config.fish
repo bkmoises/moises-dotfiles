9ae9c9fa6e236d0ebdefb2b8a9e14ba4f0498c9a
bkmoises <bk_moises@hotmail.com>
2024-07-24 11:22:56 -0300

    feature: inclusão das configurações do yazi

--- fish/config.fish
+++ fish/config.fish
@@ -1,21 +1,43 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
+
 end
 
+function yy
+    set tmp (mktemp -t "yazi-cwd.XXXXXX")
+    yazi $argv --cwd-file="$tmp"
+    if set cwd (cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+        cd -- "$cwd"
+    end
+    rm -f -- "$tmp"
+end
 
 # ALIAS
 alias vi nvim
 alias ai aichat
-alias dc "docker compose"
-alias start 'cmd.exe /c start'
+alias py python
+alias cat bat
 alias cls clear
-alias multi multipass
-alias multi-run-test "multi stop script-test && multi restore script-test.snapshot4 -d && multi start script-test && multi exec script-test -- chmod +x Documentos/script.sh && multi exec script-test -- ./Documentos/script.sh
-"
+alias ls 'exa --icons --git'
+alias ws 'cmd.exe /c start'
 
 # PATH
-# set -x PATH $PTH /home/moisesreis/.local/bin /home/moisesreis/.cargo/bin
-# set -x JAVA_HOME /usr/lib/jvm/java-11-openjdk/
-# $JAVA_HOME
-# set -x SSH_AGENT_PID (ssh-agent -c)
-set -x FIREFOX "https://download.mozilla.org/?product=firefox-devedition-latest-ssl&os=linux64&lang=en-US"
+set -x PATH $PATH /home/moisesreis/.local/bin /home/moisesreis/.cargo/bin $JAVA_HOME
+set -x JAVA_HOME /usr/lib/jvm/java-11-openjdk/
+set -x SSH_AGENT_PID (ssh-agent -c)
+set -x DOWNLOAD /mnt/c/Users/brandm10/Downloads
+set -x EDITOR nvim
+
+## Virutal Env Python
+# set VIRTUAL_ENV /home/moisesreis/Projetos/access-control/env
+
+## Variávies Temporárias
+set -x src /home/moisesreis/Work/unimedfesp-datalake/src/unimedfesp_datalake_apps/pipelines/datalake/
+set -x ddl /home/moisesreis/Work/unimedfesp-datalake/ddl/datalake/table/
+
+set -x bronze /home/moisesreis/Work/unimedfesp-datalake/src/unimedfesp_datalake_apps/pipelines/datalake/staged
+set -x silver /home/moisesreis/Work/unimedfesp-datalake/src/unimedfesp_datalake_apps/pipelines/datalake/curated/cur_progress_pub/
+set -x gold /home/moisesreis/Work/unimedfesp-datalake/src/unimedfesp_datalake_apps/pipelines/datalake/trusted/tru_datalake_fesp_pub/
+
+set -x download /mnt/c/Users/brandm10/Downloads
+set -x screenshots '/mnt/c/Users/brandm10/OneDrive - Ingram Micro/Pictures/Screenshots'
