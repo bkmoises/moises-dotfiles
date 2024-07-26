17213e11037c77b22664a80e5dc0ae64a5795d71
bkmoises <bk_moises@hotmail.com>
2024-07-26 18:34:23 -0300

    feature: correçao instação lazyvim

--- lazyvim/lazy-install.sh
+++ lazyvim/lazy-install.sh
@@ -4,7 +4,7 @@ git clone https://github.com/LazyVim/starter ~/.config/nvim
 rm -rf ~/.config/nvim/.git
 
 rm -rf ~/.config/nvim/lua/config ~/.config/nvim/lua/plugins
-cp -r ./config/ ./plugins/ ~/.config/nvim/lua/
+cp -r ./lazyvim/config/ ./lazyvim/plugins/ ~/.config/nvim/lua/
 
 # Dependencias
 LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
@@ -13,7 +13,7 @@ tar xf lazygit.tar.gz lazygit
 sudo install lazygit /usr/local/bin
 
 # Instalar Dependencias
-sudo apt install -y php ruby julia ruby-full golang php-cli liblua5.1-0-dev xclip xcel
+sudo apt install -y php ruby julia ruby-full golang php-cli liblua5.1-0-dev
 
 # Instalar composer
 curl -sS https://getcomposer.org/installer -o composer-setup.php
--- resources/conf.sh
+++ resources/conf.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-source ../resources/functions.sh
-
-loading "Definindo shell padrão"
 FISH_PATH=$(which fish)
 sudo chsh -s $FISH_PATH $USER
