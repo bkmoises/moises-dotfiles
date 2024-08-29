e633cfc1b47c369ddec37eab0b250fb8057500de
bkmoises <bk_moises@hotmail.com>
2024-08-29 20:40:34 -0300

    fix: alteração da responsabilidade do package install

---
+++ Ubuntu/install.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+./packages-install.sh
--- Ubuntu/packages-install.sh
+++ Ubuntu/packages-install.sh
@@ -1,36 +1,43 @@
 #!/bin/bash
 
-sudo apt update -y
-sudo apt upgrade -y
-sudo apt dist-upgrade -y
-sudo apt autoremove -y
+#TODO: inscluir o python3
 
+source ../resources/functions.sh
+
+# Atualização e upgrade do sistema
+info "Atualizando e fazendo upgrade do sistema..."
+sudo apt update -y || error "Erro ao executar apt update"
+sudo apt upgrade -y || error "Erro ao executar apt upgrade"
+sudo apt dist-upgrade -y || error "Erro ao executar apt dist-upgrade"
+sudo apt autoremove -y || error "Erro ao executar apt autoremove"
+
+# Lista de pacotes a serem instalados
 packages=(
   build-essential
   cargo
-	nodejs
-	npm
-	python3
+  nodejs
+  npm
+  python3
   python-pip
   curl
   make
   neovim
-	ssh
-	tig
-	tmux
-	tree
-	unrar
-	unzip
-	wget
+  ssh
+  tig
+  tmux
+  tree
+  unrar
+  unzip
+  wget
   bpytop
   diodon
-	alacritty
-	discord
-	fish
-	fzf
+  alacritty
+  discord
+  fish
+  fzf
   flameshot
-	qbittorrent
-	vlc
+  qbittorrent
+  vlc
   bat
   exa
   snapd
@@ -38,23 +45,27 @@ packages=(
   unar
   jq
   ripgrep
-  fzf
   fd-find
   zoxide
   xclip
   gnome-tweaks
   gnome-shell-extensions
+  cmake
 )
 
-sudo apt install -y "${packages[@]}" 2> error.log
+# Instalação dos pacotes
+info "Instalando pacotes..."
+sudo apt install -y "${packages[@]}" 2>/tmp/error.log || error "Erro ao instalar pacotes. Verifique error.log para mais detalhes."
 
-echo "Instalando Spotify"
-sudo snap install spotify
+# Instalação do Spotify via snap
+info "Instalando Spotify..."
+sudo snap install spotify || error "Erro ao instalar Spotify"
 
-echo "Instalando Yazi"
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
-rustup update
-cargo install --locked yazi-fm yazi-cli
-cargo install --locked --git https://github.com/sxyazi/yazi.git yazi-fm yazi-cli
+# # Instalação do Yazi via cargo
+# info "Instalando Yazi..."
+# curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y || error "Erro ao instalar Rust"
+# rustup update || error "Erro ao atualizar Rust"
+# cargo install --locked yazi-fm yazi-cli || error "Erro ao instalar Yazi"
+# cargo install --locked --git https://github.com/sxyazi/yazi.git yazi-fm yazi-cli || error "Erro ao instalar Yazi via Git"
 
-echo "Pacotes instalados com sucesso!"
+info "Pacotes instalados com sucesso!"
