5507aa2fd8403ed735ee97349a51fddeb9f9f82f
bkmoises <bk_moises@hotmail.com>
2024-08-30 00:02:51 -0300

    feature: inclusão das configurações do conky

--- Ubuntu/conf.sh
+++ Ubuntu/conf.sh
@@ -7,7 +7,7 @@ dconf load / <./Ubuntu/mysettings.conf
 
 info "Aplicando configurações do Alacritty..."
 mkdir -p ~/.config/alacritty
-cp -f ./alacritty/alacritty.yml ~/.config/alacritty/
+cp -f ./.config/alacritty/* ~/.config/alacritty/
 
 info "Aplicando configurações do Flameshot..."
 mkdir -p ~/.config/flameshot
@@ -35,5 +35,9 @@ cp -r ./.triggers/* ~/.triggers/
 
 info "Movendo script de controle do monitor..."
 sudo cp -f ./resources/monitor-controller /usr/bin/
+
+info "Aplicando Papel de Parede"
+mkdir -p ~/Imagens/"Papéis de parede"
+cp -r ../resources/"Papéis de parede"/* ~/Imagens/"Papéis de parede"/
 
 echo "Configurações aplicadas com sucesso!"
